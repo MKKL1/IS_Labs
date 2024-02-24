593f13688a8b988b67a29c9bc0059696ab0a0e51
MKKL1 <Mkkl3301@gmail.com>
2024-02-24 18:20:51 +0100

    Lab1 zad 1.1-1.4

---
+++ IS_Lab1_XML/XMLReadWithDOMApproach.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml;
+
+namespace IS_Lab1_XML
+{
+    internal class XMLReadWithDOMApproach
+    {
+        internal static void Read(string xmlpath)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(xmlpath);
+            string postac;
+            string sc;
+            int count = 0;
+            var drugs = doc.GetElementsByTagName("produktLeczniczy");
+            Dictionary<string, string> nazwy = new();
+            foreach (XmlNode d in drugs)
+            {
+                postac = d.Attributes.GetNamedItem("nazwaPostaciFarmaceutycznej").Value;
+                sc = d.Attributes.GetNamedItem("nazwaPowszechnieStosowana").Value;
+                if (nazwy.ContainsKey(sc))
+                {
+                    if(nazwy[sc] != postac) //Jezeli postac inna niz juz sprawdzona
+                        count++;
+                } else nazwy.Add(sc, postac);
+            }
+            Console.WriteLine("Liczba produktów leczniczych w o takiej samej nazwie powszechnej, ale o różnych postaciach {0}", count);
+        }
+    }
+}
---
+++ IS_Lab1_XML/XMLReadWithSAXApproach.cs
@@ -0,0 +1,39 @@
+﻿using System.Xml;
+
+namespace IS_Lab1_XML
+{
+    internal class XMLReadWithSAXApproach
+    {
+        internal static void Read(string xmlpath)
+        {
+            // konfiguracja początkowa dla XmlReadera
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.IgnoreComments = true;
+            settings.IgnoreProcessingInstructions = true;
+            settings.IgnoreWhitespace = true;
+            // odczyt zawartości dokumentu
+            XmlReader reader = XmlReader.Create(xmlpath, settings);
+            // zmienne pomocnicze
+            int count = 0;
+            string postac = "";
+            string sc = "";
+            reader.MoveToContent();
+            Dictionary<string, string> nazwy = new();
+            // analiza każdego z węzłów dokumentu
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element && reader.Name == "produktLeczniczy")
+                {
+                    postac = reader.GetAttribute("nazwaPostaciFarmaceutycznej");
+                    sc = reader.GetAttribute("nazwaPowszechnieStosowana");
+                    if (nazwy.ContainsKey(sc))
+                    {
+                        if(nazwy[sc] != postac) //Jezeli postac inna niz juz sprawdzona
+                            count++;
+                    } else nazwy.Add(sc, postac);
+                }
+            }
+            Console.WriteLine("Liczba produktów leczniczych w o takiej samej nazwie powszechnej, ale o różnych postaciach {0}", count);
+        }
+    }
+}
